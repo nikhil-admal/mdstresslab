1247f2104bd9ba8b4835782006d668986ee029fa
nikhil-admal <admal@illinois.edu>
2025-02-03 17:24:47 -0600

    Rigidity matrix implemented

---
+++ include/MDStressLab++/Rigidity.h
@@ -0,0 +1,16 @@
+//
+// Created by Nikhil Chandra Admal on 1/29/25.
+//
+
+#ifndef MDSTRESSLAB_RIGIDITY_H
+#define MDSTRESSLAB_RIGIDITY_H
+
+
+
+class Rigidity {
+
+};
+
+
+
+#endif //MDSTRESSLAB_RIGIDITY_H
